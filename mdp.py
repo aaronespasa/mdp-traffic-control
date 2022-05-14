59f8b70e4b4f5dd76571a6458b5225fce45b9f7b
aaronespasa <aaespasa@gmail.com>
2022-05-14 20:10:20 +0200

    Refactoring the code

--- mdp.py
+++ mdp.py
@@ -7,8 +7,8 @@ This file uses the CSV file data.csv to train the MDP located on the file mdp.py
 Copyright (c), Aarón Espasandín Geselmann, Marina Buitrago Pérez - All Rights Reserved
 """
 
-import json
-import pandas as pd
+from json import load
+from pandas import read_csv
 
 class TrafficControlMdp:
     """Traffic Control Stochastic Markov Decision Process"""
@@ -24,7 +24,7 @@ class TrafficControlMdp:
                     - Actions (list): List of the actions of the MDP.
         """
         with open(config_file, 'r', encoding="utf-8") as config_file_descriptor:
-            config_dict = json.load(config_file_descriptor)
+            config_dict = load(config_file_descriptor)
             # the previous states do not contain the final state
             self.prev_states = config_dict['states']
             self.prev_states.remove(config_dict['final_state'])
@@ -32,19 +32,23 @@ class TrafficControlMdp:
             self.actions = config_dict['actions']
 
             # modify the index of all the probabilities to be prev_state
-            self.probabilities_e = pd.read_csv(config_dict['probabilities_e'])
+            self.probabilities_e = read_csv(config_dict['probabilities_e'])
             self.probabilities_e.columns = ['prev_state'] + list(self.probabilities_e.columns[1:])
             self.probabilities_e.set_index('prev_state', inplace=True)
 
-            self.probabilities_w = pd.read_csv(config_dict['probabilities_w'])
+            self.probabilities_w = read_csv(config_dict['probabilities_w'])
             self.probabilities_w.columns = ['prev_state'] + list(self.probabilities_w.columns[1:])
             self.probabilities_w.set_index('prev_state', inplace=True)
 
-            self.probabilities_n = pd.read_csv(config_dict['probabilities_n'])
+            self.probabilities_n = read_csv(config_dict['probabilities_n'])
             self.probabilities_n.columns = ['prev_state'] + list(self.probabilities_n.columns[1:])
             self.probabilities_n.set_index('prev_state', inplace=True)
 
         self.current_action = None
+        self.reset_expected_values_and_best_actions()
+    
+    def reset_expected_values_and_best_actions(self):
+        """Resets the values and the best_actions arrays."""
         self.values = [0] * len(self.new_states)
         self.best_actions = [""] * len(self.new_states)
 
@@ -57,9 +61,7 @@ class TrafficControlMdp:
             epsilon (float): Threshold for the convergence of the algorithm.
             reset (bool): If True, the values and the best_actions arrays are filled with 0s.
         """
-        if reset:
-            self.values = [0] * len(self.new_states)
-            self.best_actions = [""] * len(self.new_states)
+        if reset: self.reset_expected_values_and_best_actions()
 
         # set the difference between values with a value bigger than epsilon
         values_difference = epsilon + 1
@@ -74,35 +76,40 @@ class TrafficControlMdp:
                 best_action = None
                 value_old = self.values[prev_state_idx]
                 value_tmp = float('inf') # Temporary value used to find the minimum value
-                
+
                 # Obtain the minimum expected value for prev_state looping over all the actions
                 # and save the value in the variable value_tmp
                 for action in self.actions:
                     action_cost = self.get_cost(prev_state, action)
-                    
+
                     # sum(P(s'|s, a) * V(s')) -> sum all the probabilities of the next states * expected values
                     new_state_probability = 0
                     for new_state_idx, new_state in enumerate(self.new_states):
                         new_state_probability += self.get_probability_state(prev_state, new_state, action) * \
                                                     self.values[new_state_idx]
-                    
+
                     # V(s) = min(c(a) + sum(probabilities * V(s')))
                     if value_tmp > action_cost + new_state_probability:
                         # update the temporal value
                         value_tmp = action_cost + new_state_probability
                         # assign the new best action
                         best_action = action
-                
+
                 # Update the best action (π(s)) and the expected value of the state (V(s))
                 self.best_actions[prev_state_idx] = best_action
                 self.values[prev_state_idx] = value_tmp
 
                 # Compute the difference to know if another iteration has to be done
                 values_difference = max(values_difference, abs(value_old - value_tmp))
-        
+
+        self.print_expected_values()
+        self.save_policy(policy_txt_file)
+
+    def print_expected_values(self):
+        """Prints the expected values of the states."""
         for prev_state, value in zip(self.prev_states, self.values):
             print(f"V({prev_state}) = {value}")
-
+        
         for prev_state_idx, prev_state in enumerate(self.prev_states):
             if prev_state == "LLH":
                 print(f"LLH: Best action should be E, but it actually is {self.best_actions[prev_state_idx]}")
@@ -111,6 +118,12 @@ class TrafficControlMdp:
             elif prev_state == "LHL":
                 print(f"LHL: Best action should be N, but it actually is {self.best_actions[prev_state_idx]}")
 
+    def save_policy(self, policy_txt_file:str):
+        """Saves the optimal policy in a text file.
+
+        Args:
+            policy_txt_file (str): Path to the file where the optimal policy will be saved.
+        """
         with open(policy_txt_file, "w", encoding="utf-8") as policy_file:
             policy_file.write(str(self.best_actions))
 
@@ -129,33 +142,6 @@ class TrafficControlMdp:
         
         return prev_state.count("H") * 10
 
-        # return 1
-        # if prev_state == "HHH":
-        #     return 5
-        
-        # action_idx = self.actions.index(action)
-
-        # if prev_state.count("H") == 2 and prev_state[action_idx] == "L":
-        #     return 10
-        
-        # if prev_state.count("H") == 1 and prev_state[action_idx] == "H":
-        #     return 0
-        
-        # return 2
-
-
-        # Initially, the associated cost of all actions will be 0
-        # as there are no preferences
-        # if self.current_action is None:
-        #     return 1
-
-        # # if the action is not changed
-        # if action == self.current_action:
-        #     return 0
-
-        # # if the action has changed
-        # return 1
-
     def get_probability_state(self, prev_state:str, new_state:str, action:str):
         """
         Returns the probability of going to the new state
@@ -163,7 +149,7 @@ class TrafficControlMdp:
         """
         if action not in self.actions:
             raise ValueError('Action not in the list of actions.')
-        # ["W", "N", "E"]
+
         if action == 'E':
             return self.probabilities_e.loc[prev_state, new_state]
 
@@ -173,10 +159,9 @@ class TrafficControlMdp:
         if action == 'N':
             return self.probabilities_n.loc[prev_state, new_state]
 
-        raise ValueError('Unexpected error.')
+        raise ValueError('Actions weren\'t correctly defined in the config.json file.')
 
 
 if __name__ == "__main__":
-     # Declare the MDP
     mdp = TrafficControlMdp("config.json")
     mdp.value_iteration("optimal_policy2.txt", epsilon=0.001, reset=True)
